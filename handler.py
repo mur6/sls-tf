904a15405528147b899dd5ac97ca18a7f9c2894c
MURAKI Taichi <muraki.taichi@gmail.com>
2019-12-25 10:30:51 +0900

    fix

--- handler.py
+++ handler.py
@@ -5,13 +5,18 @@ except ImportError:
 
 
 import json
-import tensorflow as tf
+#import tensorflow as tf
+import yaml
+#import pandas as pd
+#import numpy as np
 
 
 def get(event, context):
-    a = tf.random.normal([2, 2])
-    js = json.dumps(a.numpy().tolist())
-    print(f"Returned json: {js}")
+    # a = tf.random.normal([2, 2])
+    # js = json.dumps(a.numpy().tolist())
+    # print(f"Returned json: {js}")
+    d = {"a": 1, "b": 2}
+    js = yaml.dump(d)
     response = {"statusCode": 200, "body": js}
     return response
 
